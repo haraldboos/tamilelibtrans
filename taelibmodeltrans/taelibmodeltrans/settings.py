33627fd63817946d5f514feb5463685c40a335b2
haraldboos <haraldboos16@gmail.com>
2024-04-06 22:35:16 +0530

    banner and footer

--- taelibmodeltrans/taelibmodeltrans/settings.py
+++ taelibmodeltrans/taelibmodeltrans/settings.py
@@ -36,7 +36,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'jazzmin',
-    # 'modeltranslation',
+    'modeltranslation',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
