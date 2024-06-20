417d41c3a3523b1f625ecd54444b70d075941a6b
haraldboos <haraldboos16@gmail.com>
2024-06-20 22:35:46 +0530

    hover

--- taelibmodeltrans/taelimodel/apps.py
+++ taelibmodeltrans/taelimodel/apps.py
@@ -1,6 +1,3 @@
-from django.apps import AppConfig
-
-
-class TaelimodelConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'taelimodel'
+version https://git-lfs.github.com/spec/v1
+oid sha256:49fb8683565fe1c9f865781170d06f673bc78a9f4fc21b35cffb833db8ef8de4
+size 152
